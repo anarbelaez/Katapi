5fd2cc179a02c6c6166b5ee01dabbe64df7dc1e8
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-12 19:46:21 -0500

    removed space of task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,5 +5,4 @@ class Task < ApplicationRecord
   validates :name, :status, :priority, :difficulty, :due_date, :goal, presence: true
   validates :name, length: { minimum: 5 }
   validates :description, length: { maximum: 200 }
-
 end
