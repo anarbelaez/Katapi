c5474003863e65cdef6669fcc24349d380070e48
Maria Antonieta Pereira <antonietap0@gmail.com>
2022-10-17 17:23:52 -0500

    add actions in task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,51 @@
 class TasksController < ApplicationController
-  def new
+  before_action :set_goal, only: %i[new create]
+  before_action :set_task, only: %i[show edit update destroy]
+  def index
+    @tasks = Task.all
   end
 
   def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.goal = @goal
+    @task.save
+    if @task.save
+      redirect_to goal_tasks_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(tasks_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to goal_tasks_path(@task.goal), status: :see_other
+  end
+
+  private
+  def tasks_params
+    params.require(:task).permit(:name, :status, :priority, :difficulty, :due_date, :goal_id)
+  end
+
+  def set_goal
+    @goal = Goal.find(params[:goal_id])
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
