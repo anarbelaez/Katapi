60a143239e465bb14824cd974be4befad34ace66
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-19 19:53:31 -0500

    fixed sidebar behavior

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
   def activity
+    # TODO:
+    # Mostrar las tareas - calendario
+    # Mostrar rendimiento
+    # Mostar las goals
   end
 
   def show
+    @name = current_user.name
+    # Mi informacion
+    # Editar mi perfil
+    # Notificaciones
   end
 end
