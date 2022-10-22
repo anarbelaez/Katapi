b79ae2ea26861ae455a7a3e56fa86da6ae1a7ef3
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-21 19:38:39 -0500

    added layout to the  home page

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: "./app/javascript/application.js",
+    pages: "./app/javascript/pages.js"
   },
   output: {
     filename: "[name].js",
