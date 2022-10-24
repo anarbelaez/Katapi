fc603c4e847e0cdb80609b86a50ae2f28faf5082
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-23 20:22:21 -0500

    fixed some problems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,11 @@ class UsersController < ApplicationController
     @tasks = @user.tasks
     @goals = @user.goals
 
-    @seed_percentage = (@user.seed_count.fdiv(@goals.count) * 100).round
-    @sapling_percentage = (@user.sapling_count.fdiv(@goals.count) * 100).round
-    @mature_tree_percentage = (@user.mature_tree_count.fdiv(@goals.count) * 100).round
-
+    if @goals.count > 0
+      @seed_percentage = (@user.seed_count.fdiv(@goals.count) * 100).round
+      @sapling_percentage = (@user.sapling_count.fdiv(@goals.count) * 100).round
+      @mature_tree_percentage = (@user.mature_tree_count.fdiv(@goals.count) * 100).round
+    end
     # def search
     #   if params[:query].present?
     #     @query = params[:query]
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,7 +2,7 @@ class Goal < ApplicationRecord
   include PgSearch::Model
 
   belongs_to :user
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   has_one_attached :photo
   has_rich_text :rich_body
 
