a7c0127923b7ffedb08c41567579c93196255747
camilo ardila <miloardila98@gmail.com>
2022-10-24 15:44:03 -0500

    solve errors on index view and show of task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,11 @@ class UsersController < ApplicationController
     @tasks = @user.tasks
     @goals = @user.goals
 
-    @seed_percentage = (@user.seed_count.fdiv(@goals.count) * 100).round || 0
-    @sapling_percentage = (@user.sapling_count.fdiv(@goals.count) * 100).round || 0
-    @mature_tree_percentage = (@user.mature_tree_count.fdiv(@goals.count) * 100).round || 0
+    if @goals.count > 0
+      @seed_percentage = (@user.seed_count.fdiv(@goals.count) * 100).round
+      @sapling_percentage = (@user.sapling_count.fdiv(@goals.count) * 100).round
+      @mature_tree_percentage = (@user.mature_tree_count.fdiv(@goals.count) * 100).round
+    end
 
     # def search
     #   if params[:query].present?
