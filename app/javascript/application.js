cc75d986b06459d2873ab697ef8344b716c08297
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-28 11:19:25 -0500

    added sweetalert

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,13 @@ import "./bootstrap_js/modal_for_goal"
 import "bootstrap"
 import "trix"
 import "@rails/actiontext"
+
+import { initSweetalert } from './plugins/init_sweetalert';
+
+initSweetalert('#sweet-alert-demo', {
+  title: "A nice alert",
+  text: "This is a great alert, isn't it?",
+  icon: "success"
+}, (value) => {
+  console.log(value);
+});
