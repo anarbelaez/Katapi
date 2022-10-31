8bc2fb7027c46c18e2d797df2e8ab69fc4970f34
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-31 18:25:41 -0500

    removed some lines

--- app/javascript/controllers/refresh_goals_list_controller.js
+++ app/javascript/controllers/refresh_goals_list_controller.js
@@ -1,13 +1,8 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "form", "list", "searchInput" ]
 
   connect() {
-    console.log("Hola Ana Mariita");
-    console.log(this.element);
-    console.log(this.formTarget);
-    console.log(this.listTarget);
-    console.log(this.searchInputTarget);
+
   }
 }
